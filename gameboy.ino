32c650ac1d2cfc58abdfe1a15a72e58c89f9e321
srivatsan kalyanasundaram <srivatsan.kalyan@gmail.com>
2022-12-15 20:04:30 +0100

    testing for just one button

--- gameboy.ino
+++ gameboy.ino
@@ -12,6 +12,7 @@ int btn6 = 17;
 int lives = 3;
 int score = 0;
 int topScore = 0;
+int flag = 1; //Flag=1 starts game, flag =2
 
 int correctButton = 0;
 int timer = millis();
@@ -26,50 +27,81 @@ void saveTopScore(int score);
 
 void setup()
 {
+    pinMode(btn1, INPUT_PULLUP);
+    Serial.begin(9600);
     lcd.init();
+    Serial.println("started");
 }
 
 void loop()
 {
     dispMainScreen();
-    if(readButton()) // checks if any button is pressed
+    delay(10);
+    if(readButton()){ // checks if any button is pressed
+        lives = 3;
         startGame();
+        
+    }
 }
 
-bool startGame(){
-    while (lives != 0){
+bool startGame()
+    {
+    lcd.clear();
+    Serial.println("into screen");
+    while (lives > 0)
+        {
+        flag=1;
         correctButton = dispGame();
+        int buttonPressed ;
         timer = millis();
-        while(millis()-timer < waitTime){
-            if (correctButton == readButton() ){
-                lives ++;
-                score ++;
-                break;
+        while(millis()-timer < waitTime && flag==1)
+            {
+             Serial.println("Entered while loop");
+            if(buttonPressed = readButton()){
+              if(correctButton == buttonPressed)
+              {
+                 lives ++;
+                  score ++;
+                  Serial.println("breaked");
+                flag=0;
+              }
+              
+             } 
             }
-        }
-        lives --;
-    }
+            lives --;
+            }
+            
     topScore = score;
-    saveTopScore(topScore);
+    
+    // saveTopScore(topScore);
+    lcd.clear();
     timer = millis();
     while(millis()-timer < waitTime){
         dispGameEnd();
+        delay(10);
     }
+    score = 0;
+    lcd.clear();
 }
 
 int readButton(){
-    if (digitalRead(btn1))
-        return btn1;
-    if (digitalRead(btn2))
-        return btn2;
-    if (digitalRead(btn3))
-        return btn3;
-    if (digitalRead(btn4))
-        return btn4;
-    if (digitalRead(btn5))
-        return btn5;
-    if (digitalRead(btn6))
-        return btn6;
+    if (!digitalRead(btn1)){
+      delay(75);
+      while(!digitalRead(btn1)){delay(1);}
+      Serial.println("button pressed");
+      return btn1;
+    }
+        
+//    if (digitalRead(btn2))
+//        return btn2;
+//    if (digitalRead(btn3))
+//        return btn3;
+//    if (digitalRead(btn4))
+//        return btn4;
+//    if (digitalRead(btn5))
+//        return btn5;
+//    if (digitalRead(btn6))
+//        return btn6;
     return 0;
 }
 
@@ -77,23 +109,24 @@ void dispMainScreen(){
     lcd.backlight();
     
     lcd.setCursor(0, 0);
-    lcd.print("Press any button to start");
+    lcd.print("Press any button");
     lcd.setCursor(1, 3);
-    lcd.print(" to start");
+    lcd.print("   to start");
 }
 
 void dispGameEnd(){
+    
     lcd.backlight();
     
-    lcd.setCursor(0, 4);
+    lcd.setCursor(4, 0);
     lcd.print("GAME OVER");
-    lcd.setCursor(1, 0);
-    lcd.print("top-");
-    lcd.setCursor(1, 5);
+    lcd.setCursor(0, 1);
+    lcd.print("top:");
+    lcd.setCursor(5, 1);
     lcd.print(score);
-    lcd.setCursor(1, 8);
-    lcd.print("now-");
-    lcd.setCursor(1, 13);
+    lcd.setCursor(8, 1);
+    lcd.print("now:");
+    lcd.setCursor(13, 1);
     lcd.print(score);
 
 
@@ -102,45 +135,51 @@ void dispGameEnd(){
 int dispGame(){
     lcd.backlight();
     lcd.setCursor(0, 0);
-    lcd.print("score-");
-    lcd.setCursor(0, 7);
+    lcd.print("score:");
+    lcd.setCursor(7, 0);
     lcd.print(score);
 
-    int randNo = random(6);
-    switch (randNo)
-    {
-    case 1:
-        lcd.setCursor(1, 0);
-        lcd.print("1");
-        return bt1;
-        break;
-    case 2:
-        lcd.setCursor(1, 2);
-        lcd.print("2");
-        return bt2;
-        break;
-    case 3:
-        lcd.setCursor(1, 4);
-        lcd.print("3");
-        return bt3;
-        break;
-    case 4:
-        lcd.setCursor(1, 6);
-        lcd.print("4");
-        return bt4;
-        break;
-    case 5:
-        lcd.setCursor(1, 8);
-        lcd.print("5");
-        return bt5;
-        break;
-    case 6:
-        lcd.setCursor(1, 10);
-        lcd.print("6");
-        return bt6;
-        break;
-    
-    default:
-        break;
-    }
+lcd.setCursor(10, 0);
+    lcd.print(lives);
+
+    lcd.setCursor(1, 1);
+    lcd.print("1");
+    return btn1;
+//    int randNo = random(6);
+//    switch (randNo)
+//    {
+//    case 1:
+//        lcd.setCursor(1, 0);
+//        lcd.print("1");
+//        return btn1;
+//        break;
+//    case 2:
+//        lcd.setCursor(1, 2);
+//        lcd.print("2");
+//        return btn2;
+//        break;
+//    case 3:
+//        lcd.setCursor(1, 4);
+//        lcd.print("3");
+//        return btn3;
+//        break;
+//    case 4:
+//        lcd.setCursor(1, 6);
+//        lcd.print("4");
+//        return btn4;
+//        break;
+//    case 5:
+//        lcd.setCursor(1, 8);
+//        lcd.print("5");
+//        return btn5;
+//        break;
+//    case 6:
+//        lcd.setCursor(1, 10);
+//        lcd.print("6");
+//        return btn6;
+//        break;
+//    
+//    default:
+//        break;
+//    }
 }
